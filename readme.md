90704baace014c7b2146548416834d8bcf7ab72a
Rahul Bothra <bothra2@illinois.edu>
2023-10-09 00:29:01 +0000

    initial commit

---
+++ readme.md
@@ -0,0 +1,19 @@
+# Azure Blob Throughput tests
+Scripts to test thoughput offered by Azure blob storage.
+
+## Setup
+1. Create an Azure Virtual Machine(s) and run the following commands,
+```
+pip install azure.core
+pip install azure-storage-blob==2.1.0
+git clone https://github.com/rhl-bthr/azure-blob-throughput
+```
+
+2. Create an Azure Storage Account and a container within, and configure `STORAGE_ACCOUNT_NAME`, `STORAGE_ACCOUNT_KEY`, `CONTAINER_NAME`, `BLOB_NAME` in `main.py`.
+
+## Usage
+```
+python3 main.py <config_name>
+```
+Results are stored in `results/`.
+Configurable parameters include `NUM_ITERS`, `FILE_SIZE`, `DATA_FILE_NAME`, `CONFIG`
---
+++ test.py
@@ -0,0 +1,49 @@
+from azure.storage.blob import AppendBlobService
+from datetime import datetime
+import os
+import sys
+import json
+
+NUM_ITERS = 5 # Number of iterations to run for this experiment
+FILE_SIZE = 1024 # File size to send in each iteration (in MB)
+DATA_FILE_NAME = "data-file" # Name of the temporary file that gets created to send
+CONFIG = "default" # Result 
+
+if len(sys.argv) > 1:
+    CONFIG = sys.argv[1]
+TIME_TAKEN = []
+
+STORAGE_ACCOUNT_NAME = "<STORAGE_ACCOUNT_NAME>"
+STORAGE_ACCOUNT_KEY = "<STORAGE_ACCOUNT_KEY>"
+CONTAINER_NAME = "<CONTAINER_NAME>"
+BLOB_NAME = "<BLOB_NAME>"
+
+def append_files_to_blob():
+    service = AppendBlobService(account_name=STORAGE_ACCOUNT_NAME, 
+            account_key=STORAGE_ACCOUNT_KEY)
+    service.append_blob_from_path(container_name=CONTAINER_NAME, blob_name=BLOB_NAME, file_path = DATA_FILE_NAME)
+
+
+
+os.system("head -c " + str(FILE_SIZE) + "M </dev/urandom > " + DATA_FILE_NAME)
+for i in range(NUM_ITERS):
+    start_time = datetime.now()
+    append_files_to_blob()
+    end_time = datetime.now()
+
+    time_diff = int((end_time - start_time).total_seconds() * 1000)
+    print("Iter", i, "time taken:", time_diff)
+    TIME_TAKEN.append(time_diff)
+os.system("rm " + DATA_FILE_NAME)
+
+average_time = sum(TIME_TAKEN)/len(TIME_TAKEN)
+print("Average time:", average_time)
+print("Average bandwidth:", FILE_SIZE / average_time, "MB/s")
+
+results = {}
+results["config"] = CONFIG
+results["times"] = TIME_TAKEN
+results["size"] = FILE_SIZE
+results["iters"] = NUM_ITERS
+results["average"] = average_time
+json.dump(results, open("results/" + CONFIG + ".json", "w"), indent = 4)
